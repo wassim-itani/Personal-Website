3725f995b65d3193b9b5402403c74e5c9897a745
Wassim Itani <wassim.j.itani@gmail.com>
2023-02-19 21:48:51 +0200

    Edit skills text

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
           <p>
             I'm knowledgeable in various front-end technologies such as{" "}
             <span className="notice">
-              HTML5, CSS3, JavaScript (ES6+), TypeScript, Sass, tailwind css, React, Vue, Redux, Git, GitLab
+              HTML5, CSS3, JavaScript (ES6+), TypeScript, Sass, React, Vue, Redux, Git, GitLab
             </span>
             , etc. In addition to some back-end technologies like{" "}
             <span className="notice">PHP</span> and{" "}
